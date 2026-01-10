50b6995cc114c5c1c03227b7c38d836999800117
ConnerJAmmo <CJAMMONS@STUDENT.FULLSAIL.EDU>
2026-01-09 23:47:24 -0800

    Implemented wall jumping
    implemented wall jumping into the player controller via some raycasting

--- Project Folder/P&Pgame/Assets/Scripts/PlayerCont.cs
+++ Project Folder/P&Pgame/Assets/Scripts/PlayerCont.cs
@@ -1,3 +1,5 @@
+using UnityEditor;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class PlayerCont : MonoBehaviour
@@ -6,15 +8,25 @@ public class PlayerCont : MonoBehaviour
 
     [SerializeField] LayerMask ignoreLayer;
 
-
+[Header("---- Stats ----")]
 [Range(1,10)][SerializeField] int HP;
     [Range(1,10)][SerializeField] int speed;
     [Range(2,5)][SerializeField] int sprintMod;
+    [Header("---- Jump ----")]
     [Range(8,20)][SerializeField] int jumpSpeed;
     [Range(1,4)][SerializeField] int jumpMax;
-     [Range(1,100)][SerializeField] int gravity;
+    [Range(8,20)][SerializeField] int wallJumpSpeed;
+    [Range(1,4)][SerializeField] int wallJumpPush;
+    [Range(1,4)][SerializeField] int wallJumpMax;
+     [Range(1,2)][SerializeField] float wallCheckDis;
+    [Header("---- Physics ----")]
+    [Range(1,100)][SerializeField] int gravity;
 
     int jumpCount;
+    int wallJumpCount;
+    RaycastHit wallJumpHit;
+    bool wallJumpPosib;
+    bool wasGrounded;
     int HPOrig;
    UnityEngine.Vector3 moveDir;
    UnityEngine.Vector3 playerVel;
@@ -34,16 +46,21 @@ public class PlayerCont : MonoBehaviour
 
      void Movement()
     {
+
+        wasGrounded = controller.isGrounded;
         moveDir = Input.GetAxis("Horizontal") * transform.right + Input.GetAxis("Vertical") * transform.forward;
         controller.Move(moveDir * speed * Time.deltaTime);
-        
+ 
         jump();
         controller.Move(playerVel * Time.deltaTime);
 
         if(controller.isGrounded)
         {
           jumpCount = 0;  
-          playerVel = UnityEngine.Vector3.zero;
+          wallJumpCount = 0;
+          playerVel.x = 0;
+          playerVel.z = 0;
+          playerVel.y = -2f;
         }
         else
         {
@@ -53,6 +70,17 @@ public class PlayerCont : MonoBehaviour
 
     void jump()
     {
+        if(Input.GetButtonDown("Jump") && !wasGrounded && wallJumpCount < wallJumpMax)
+        {
+           WallCheck();
+           if(wallJumpPosib)
+            {
+                playerVel.y = wallJumpSpeed;
+                playerVel.x = wallJumpHit.normal.x * wallJumpPush;
+                wallJumpCount++;
+                return;
+            }
+        }
         if(Input.GetButtonDown("Jump") && jumpCount < jumpMax)
         {
             playerVel.y = jumpSpeed;
@@ -71,4 +99,11 @@ public class PlayerCont : MonoBehaviour
             speed /= sprintMod;
         }
     }
+
+    private void WallCheck()
+    {
+        wallJumpPosib = Physics.Raycast(transform.position, transform.right, out wallJumpHit, wallCheckDis) ||
+        Physics.Raycast(transform.position, -transform.right, out wallJumpHit, wallCheckDis);
+    }
+
 }
