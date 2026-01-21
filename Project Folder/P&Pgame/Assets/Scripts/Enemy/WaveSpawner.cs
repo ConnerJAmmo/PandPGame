86952d342069aff6bd4aa46a723c293315b81da4
ConnerJAmmo <CJAMMONS@STUDENT.FULLSAIL.EDU>
2026-01-21 13:23:07 -0800

    Reverted to ConnerWaveSpawner

--- Project Folder/P&Pgame/Assets/Scripts/Enemy/WaveSpawner.cs
+++ Project Folder/P&Pgame/Assets/Scripts/Enemy/WaveSpawner.cs
@@ -4,70 +4,6 @@ using System.Data;
 
 public class WaveSpawner : MonoBehaviour
 {
-    [System.Serializable]
-    public class Wave
-    {
-        public GameObject wavePrefab;
-        public float timeBetweenSpawns = 30;
-    }
-
-    public Wave[] waves; 
-    private int currentWaveIndex = 0;
-
-    private void Start()
-    {
-        gameManager.instance.SetWaveCountUI(waves.Length);
-        gameManager.instance.SetActiveWaveUI(currentWaveIndex + 1);
-    }
-
-    void Update()
-    {
-        if (!gameManager.instance.waveActive && currentWaveIndex < waves.Length)
-        {
-            StartCoroutine(SpawnWaveRoutine(waves[currentWaveIndex]));
-        }
-    }
-
-    private IEnumerator SpawnWaveRoutine(Wave wave)
-    {
-        Debug.Log("Spawning Wave: " + (currentWaveIndex + 1));
-        gameManager.instance.waveActive = true;
-        gameManager.instance.SetActiveWaveUI(currentWaveIndex + 1);
-
-        foreach (Transform childContainer in wave.wavePrefab.transform)
-        {
-            // 1. Instantiate the container
-            GameObject containerInstance = Instantiate(childContainer.gameObject, transform.position + childContainer.localPosition, childContainer.rotation);
-
-            // 2. Use a while loop to avoid skipping children when unparenting
-            // This ensures every single child is moved out safely
-            while (containerInstance.transform.childCount > 0)
-            {
-                Transform enemy = containerInstance.transform.GetChild(0);
-                enemy.SetParent(null); // Move enemy to world space
-            }
-
-            // 3. Destroy the now-empty container
-            Destroy(containerInstance);
-
-            yield return new WaitForSeconds(wave.timeBetweenSpawns);
-        }
-
-        // Wait until all enemies are dead
-        yield return new WaitUntil(() => gameManager.instance.enemyCount <= 0);
-
-        gameManager.instance.enemyCountOrig = 0;
-        gameManager.instance.updateEnemyCountTotal(0);
-
-        currentWaveIndex++;
-        gameManager.instance.waveActive = false;
-    }
-}
-
-
-/*
-public class WaveSpawner : MonoBehaviour
-{
     [SerializeField] private float countdown;
     [SerializeField] private GameObject spawnPoint;
 
@@ -141,4 +77,3 @@ public class Wave
 
     [HideInInspector] public int enemiesLeft;
 }
-*/
