f94861fedf0f9b817530aa090be3b7b2cb4088d8
Arrowheadgaming <133914921+Arrowheadgaming@users.noreply.github.com>
2026-01-27 23:48:25 -0600

    Add player upgrade system
    Add player upgrade system

--- Project Folder/P&Pgame/Assets/Scripts/Gun/GunStats.cs
+++ Project Folder/P&Pgame/Assets/Scripts/Gun/GunStats.cs
@@ -5,9 +5,13 @@ public class GunStats : ScriptableObject
 {
     public GameObject gunModel;
 
+    public string gunName;
     [Range(3, 1200)] public int shootDist;
+    public int shootDistOrig;
     [Range(1, 15)] public int shootDamage;
+    public int shootDamageOrig;
     [Range(0.1f, 4)] public float shootRate;
+    public float shootRateOrig;
 
     public int ammoCur;
     [Range(5, 50)] public int ammoMax;
