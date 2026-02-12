f9315145959ba27071289d5a2682e479315a1860
Arrowheadgaming <133914921+Arrowheadgaming@users.noreply.github.com>
2026-02-12 00:58:14 -0600

    Load screen/ Main menu
    Add the start of a Load screen and Main menu with the new logo

---
+++ Project Folder/P&Pgame/Assets/Scripts/UI/Load Screen/LevelLoader.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class LevelLoader : MonoBehaviour
+{
+    public GameObject loadingScreen; 
+    public Slider slider;
+    public TMP_Text progressText;
+
+    public void LoadLevel (int sceneIndex)
+    {
+        StartCoroutine(LoadAsynchronously(sceneIndex));
+    } 
+
+    IEnumerator LoadAsynchronously (int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+        loadingScreen.SetActive(true);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+            slider.value = progress;
+            
+            Debug.Log(progress);
+
+            float progressPr = progress * 100;
+            Debug.Log(progressPr);
+            progressText.text = progressPr.ToString("F0") + "%";
+
+            Debug.Log(progressPr);
+
+            yield return null;
+        }
+    }
+}
