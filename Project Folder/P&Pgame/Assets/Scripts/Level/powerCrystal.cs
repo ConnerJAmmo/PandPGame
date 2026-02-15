af6a7675a9d0274066ec5aa82ca56963ca2cddbc
AsherSykes <ashersykes78@gmail.com>
2026-02-15 15:33:28 -0500

    ThePowerCrystal

---
+++ Project Folder/P&Pgame/Assets/Scripts/Level/powerCrystal.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class powerCrystal : MonoBehaviour
+{
+    //Yes, the power emerald... I can feel the POWER!
+    [Header("Numbers")]
+    [SerializeField] int amount;
+
+    [Header("Audio")]
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip pickupSound;
+    [SerializeField][Range(0, 1)] private float pickupVolume = 0.5f;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    
+        IPickupGeneric pickerUpper = other.GetComponent<IPickupGeneric>();
+
+        if (pickerUpper != null)
+        {
+
+            pickerUpper.getGeneric("Power Crystal", amount);
+
+            audioSource.PlayOneShot(pickupSound);
+
+            Destroy(gameObject);
+
+        }
+    }
+}
