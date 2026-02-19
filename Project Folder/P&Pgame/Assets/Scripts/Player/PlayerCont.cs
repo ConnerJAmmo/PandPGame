4a1b1fe311d86f23d631f391471c3c2d4a4c7de8
ConnerJAmmo <CJAMMONS@STUDENT.FULLSAIL.EDU>
2026-02-18 19:56:04 -0800

    Gorge Changes + Spint

--- Project Folder/P&Pgame/Assets/Scripts/Player/PlayerCont.cs
+++ Project Folder/P&Pgame/Assets/Scripts/Player/PlayerCont.cs
@@ -50,6 +50,7 @@ public class PlayerCont : MonoBehaviour, IStore, IDamage, IPickup, IPickupKeys, 
     [SerializeField] public Transform machineGunShootPos;
     [SerializeField] public Transform m1GarandShootPos;
     [SerializeField] public Transform m1918BarShootPos;
+    [SerializeField] cameraContr camScript;
 
     [Header("Guns")]
     [SerializeField] public List<GunStats> gunList = new List<GunStats>();
@@ -289,6 +290,10 @@ public class PlayerCont : MonoBehaviour, IStore, IDamage, IPickup, IPickupKeys, 
                 playerVel.y = wallJumpSpeed;
                 playerVel.x = wallJumpHit.normal.x * wallJumpPush;
                 wallJumpCount++;
+
+                float tiltDir = Vector3.Dot(wallJumpHit.normal, transform.right) > 0 ? -1f : 1f;
+                camScript.SetWallJumpTilt(tiltDir);
+                StartCoroutine(ResetTiltAfterDelay());
                 return;
             }
         }
@@ -302,13 +307,28 @@ public class PlayerCont : MonoBehaviour, IStore, IDamage, IPickup, IPickupKeys, 
 
     void sprint()
     {
-        if(Input.GetButtonDown("Sprint"))
+        /*if(Input.GetButtonDown("Sprint"))
         {
             speed *= sprintMod;
+            camScript.SetSprintFOV();
         }
         else if (Input.GetButtonUp("Sprint"))
         {
             speed /= sprintMod;
+            camScript.ResetFOV();
+        } This sprint is for hold to sprint */
+        if(Input.GetButton("Sprint"))
+        {
+            if(speed == baseSpeed)
+            {
+                speed *= sprintMod;
+                camScript.SetSprintFOV();
+            }
+            else
+            {
+               speed /= sprintMod;
+               camScript.ResetFOV(); 
+            }
         }
     }
 
@@ -593,5 +613,11 @@ public class PlayerCont : MonoBehaviour, IStore, IDamage, IPickup, IPickupKeys, 
         {
             powerCrystals = powerCrystals + amount;
         }
+    }
+
+    IEnumerator ResetTiltAfterDelay()
+    {
+        yield return new WaitForSeconds(0.15f);
+        camScript.ResetTilt();
     }
 }
