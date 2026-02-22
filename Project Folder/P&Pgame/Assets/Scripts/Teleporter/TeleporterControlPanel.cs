02dd19d5ac4c2923152f94e2c09cc16f882b8e50
codewriter77 <Believeitllc1@gmail.com>
2026-02-22 03:08:16 -0600

    Revamped the teleporter so all electrical lines come on with each crystal inserted, added mesh colliders to all objects inside mothership change most of the texture to downgrade size, added a key, created a script for key

--- Project Folder/P&Pgame/Assets/Scripts/Teleporter/TeleporterControlPanel.cs
+++ Project Folder/P&Pgame/Assets/Scripts/Teleporter/TeleporterControlPanel.cs
@@ -1,16 +1,18 @@
+using System;
 using Unity.Burst.CompilerServices;
 using UnityEngine;
+using UnityEngine.ProBuilder.MeshOperations;
 
 public class TeleporterControlPanel : MonoBehaviour
 {
     [Header("Setup")]
     [SerializeField] string padAId = "PadA";
     [SerializeField] string padBId = "PadB";
-    [SerializeField] int requiredCrystals;
+    [SerializeField] int requiredCrystals = 7;
 
     [Header("Message")]
-    [SerializeField] string msgNotEnough = "Collect and enter 6 crystals to activate teleporter";
-    [SerializeField] string msgReady = "Press F to insert crystals";
+    [SerializeField] string msgNotEnough = "Collect crystals to activate teleporter";
+    [SerializeField] string msgReady = "Press F to insert a crystals";
     [SerializeField] string msgActivated = "Teleporter activated";
 
     [Header("Audio")]
@@ -25,9 +27,29 @@ public class TeleporterControlPanel : MonoBehaviour
     [SerializeField] PanelDoor door;
     [SerializeField] PanelScreenUI screen;
 
+    [Header("PowerLines")]
+    [SerializeField] ElectricLine[] powerLines; // Size = requiredCrystals
+    [SerializeField] AudioClip LineOnSfx;
+    [SerializeField] float lineOnVol;
+
+    int inserted = 0;
+
     bool playerInRange;
     Transform player;
     PlayerCrystalInventory inv;
+
+    private void Awake()
+    {
+        // All lines start off
+        if (powerLines != null)
+        {
+            for (int i = 0; i < powerLines.Length; i++)
+            {
+                if (powerLines[i])
+                    powerLines[i].TurnOff();
+            }
+        }
+    }
 
     private void Update()
     {
@@ -41,34 +63,86 @@ public class TeleporterControlPanel : MonoBehaviour
         else
             count = 0;
 
+        // If already active, Lock UI
         bool bothActive = PlayerTeleporter.AreBothActive(padAId, padBId);
         if (bothActive)
         {
             screen?.SetText("Teleporter online");
             return;
         }
+        
+        // If already enough crystals, activate (failsafe)
+        if (inserted >= requiredCrystals)
+        {
+            ActivateTeleporter();
+            return;
+        }
 
-        if (count < requiredCrystals)
+        //Show message
+        if (count <= 0)
         {
             screen?.SetText($"{msgNotEnough} ({count}/{requiredCrystals})");
             return;
         }
 
-        screen?.SetText($"{msgReady} ({count}/{requiredCrystals})");
+        screen?.SetText($"{msgReady} ({inserted} / {requiredCrystals})  You have: {count}");
+
+        //screen?.SetText($"{msgReady} ({count}/{requiredCrystals})");
 
         if (Input.GetKeyDown(KeyCode.F))
         {
-            if (inv && inv.ConsumeCrystals(requiredCrystals))
-            {
-                //teleporterToActivate.Activate();
-                PlayerTeleporter.ActivatePair(padAId, padBId);
-                
-                if (aud && activateSfx)
-                    aud.PlayOneShot(activateSfx, vol);
+            if (!inv) return;
 
-                screen?.SetText(msgActivated);
+            if (inv.ConsumeCrystals(1))
+            {
+                inserted++;
+
+                int lineIndex = inserted - 1;
+                if (powerLines != null && lineIndex >= 0 && lineIndex < powerLines.Length && powerLines[lineIndex])
+                    powerLines[lineIndex].TurnOn();
+
+                if (aud && LineOnSfx)
+                    aud.PlayOneShot(LineOnSfx, lineOnVol);
+
+                // If Done Activate portal
+                if (inserted >= requiredCrystals)
+                {
+                    ActivateTeleporter();
+                }
+                else
+                {
+                    // Update text immediately after insert
+                    int newCount = inv.Crystals;
+                    screen?.SetText($"{msgReady} ({inserted}/{requiredCrystals})  You have {newCount}");
+
+                }
+            }
+            else
+            {
+                // consume failed
+                screen?.SetText($"{msgNotEnough} ({inserted}/{requiredCrystals})");
             }
         }
+    }
+                    
+            
+
+    private void ActivateTeleporter()
+    {
+        // Turn on any remaining lines just in case array is shorter
+        if (powerLines != null)
+        {
+            for (int i = 0; i < powerLines.Length; i++)
+                if (powerLines[i])
+                    powerLines[i].TurnOn();
+        }
+
+        PlayerTeleporter.ActivatePair(padAId, padBId);
+
+        if (aud && activateSfx)
+            aud.PlayOneShot(activateSfx, vol);
+
+        screen?.SetText(msgActivated);
     }
 
     private void OnTriggerEnter(Collider other)
@@ -80,15 +154,16 @@ public class TeleporterControlPanel : MonoBehaviour
         player = other.transform;
         inv = other.GetComponent<PlayerCrystalInventory>();
 
+        door?.Open();
+        if (aud && audDoorOpen)
+            aud.PlayOneShot(audDoorOpen, audDoorOpenVol);
+
         int count;
         if (inv)
             count = inv.Crystals;
         else
             count = 0;
-        door?.Open();
-        if (aud && audDoorOpen)
-            aud.PlayOneShot(audDoorOpen, audDoorOpenVol);
-        screen?.SetText($"{msgNotEnough} ({count}/{requiredCrystals})");
+        screen?.SetText($"{msgNotEnough} ({inserted}/{requiredCrystals})  You have {count}");
     }
 
     private void OnTriggerExit(Collider other)
